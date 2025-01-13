3fbe62903d70e783b94ffde78d8f2efbdc5ff86b
andreatticj <andreatticj@gmail.com>
2025-01-13 20:32:35 +0100

    ADD bbdd y css, tambien configuraciones varias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,18 @@
 module eu.andreatt.proyecto2_dein {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.sql;
+    requires javafx.web;
 
+    // Exportar paquetes necesarios
+    exports eu.andreatt.proyecto2_dein.application to javafx.graphics;
+    exports eu.andreatt.proyecto2_dein.util; // Si es necesario
 
-    opens eu.andreatt.proyecto2_dein to javafx.fxml;
-    exports eu.andreatt.proyecto2_dein;
+    // Abrir paquetes para acceso reflexivo
+    opens eu.andreatt.proyecto2_dein.application to javafx.fxml;
+    opens eu.andreatt.proyecto2_dein.util to javafx.fxml;
+    opens eu.andreatt.proyecto2_dein.controllers to javafx.fxml;
+    opens eu.andreatt.proyecto2_dein.model to javafx.base;
 }
